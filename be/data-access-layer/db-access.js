ca8896e709700f97d466a93de4c6c92b35462a50
ikrigel <ikrigel@gmail.com>
2021-11-08 10:02:36 +0200

    Add files via upload

--- be/data-access-layer/db-access.js
+++ be/data-access-layer/db-access.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql')
 let con;
-
+//'mysql://b957015d3dd7cd:b775fb62@us-cdbr-east-04.cleardb.com/heroku_98eed5a993c392d?reconnect=true'
+//mysql --host=us-cdbr-east-04.cleardb.com --user=b957015d3dd7cd --password=b775fb62 --reconnect heroku_98eed5a993c392d
 function Open(app) {
     return new Promise((resolve, reject) => {
         con = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'igal_store',
+            host: 'us-cdbr-east-04.cleardb.com',
+            user: 'b957015d3dd7cd',
+            password: 'b775fb62',
+            database: 'heroku_98eed5a993c392d',
             timezone: 'utc'
         });
         con.connect((err) => {
